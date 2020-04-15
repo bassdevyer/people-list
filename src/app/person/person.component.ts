92d2502008e8261c5f96831ee21c99b673b21e3c
Miguel Torres <miguetv17@gmail.com>
2020-04-14 22:16:11 -0500

    Property binding between components

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Person} from '../person.model';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent implements OnInit {
+
+  @Input() person: Person;
+  @Input() index: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
