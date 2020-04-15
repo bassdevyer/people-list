cbc46609f325c2d9d60e8e1c4f1551b0237afd26
Miguel Torres <miguetv17@gmail.com>
2020-04-14 22:32:56 -0500

    Event binding between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,9 @@ import {Person} from './person.model';
 export class AppComponent {
   title = 'People List';
   people: Person[] = [new Person('Juan', 'Perez'), new Person('Laura', 'Juarez')];
-  nameInput: string;
-  lastNameInput: string;
 
-  onAddPerson() {
-    let person1 = new Person(this.nameInput, this.lastNameInput);
-    this.people.push(person1);
+  onPersonAdded(person: Person) {
+    this.people.push(person);
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import { PersonComponent } from './person/person.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent
+    PersonComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
