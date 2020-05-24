5553806cd90b6bdde1c0f137fb45f4f0bb780877
Miguel Torres <miguetv17@gmail.com>
2020-05-24 01:54:35 -0500

    Uso de guardianes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {PersonasComponent} from './personas/personas.component';
 import {FormComponent} from './personas/form/form.component';
 import {ErrorComponent} from './error/error.component';
 import {LoginComponent} from './login/login.component';
+import {LoginGuardian} from './login/login-guardian.service';
 
 const routes: Routes = [
-  {path: '', component: PersonasComponent},
+  {path: '', component: PersonasComponent, canActivate: [LoginGuardian]},
   {
-    path: 'personas', component: PersonasComponent, children:
+    path: 'personas', component: PersonasComponent, canActivate: [LoginGuardian], children:
       [
         {path: 'agregar', component: FormComponent},
         {path: ':id', component: FormComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DataServices} from './data.services';
 import {HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
 import {LoginService} from './login/login.service';
+import {LoginGuardian} from './login/login-guardian.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import {LoginService} from './login/login.service';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [LoggingService, PeopleService, DataServices, LoginService],
+  providers: [LoggingService, PeopleService, DataServices, LoginService, LoginGuardian],
   bootstrap: [AppComponent]
 })
 export class AppModule {
