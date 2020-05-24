f46ce9721d390739facb7bfdd2d443524a8b92d7
Miguel Torres <miguetv17@gmail.com>
2020-05-24 01:00:22 -0500

    Configuring firebase for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {PersonasComponent} from './personas/personas.component';
 import {FormComponent} from './personas/form/form.component';
 import {ErrorComponent} from './error/error.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: PersonasComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
         {path: ':id', component: FormComponent}
       ]
   },
+  {path: 'login', component: LoginComponent},
   {path: '**', component: ErrorComponent}
 ];
 
